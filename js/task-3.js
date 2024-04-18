f0301d1f842b90a1dafb98a79593a2db646933c1
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-04-18 13:52:28 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function filterArray(numbers, value) {
-  const filteredNumbers = [];
+  let filteredNumbers = [];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
       filteredNumbers.push(numbers[i]);
